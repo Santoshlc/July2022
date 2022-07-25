424df44aefd0ae9b58888d19239501bdd48eadb5
santo <santo@DESKTOP-7D9K99H>
2022-07-25 09:41:26 +0530

    "Hello GIT HUB"

--- src/com/santosh/java/SampleClass.java
+++ src/com/santosh/java/SampleClass.java
@@ -5,7 +5,10 @@ public class SampleClass {
 	public static void main(String[] args) 
 	{
 		System.out.println(" Hello Java");
-
+		
+		
+		System.out.println(" Hello Welcome to GIT HUB");
 	}
+	
 
 }
